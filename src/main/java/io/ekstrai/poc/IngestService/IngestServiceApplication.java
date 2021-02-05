074479209223ac4a725cb0bc0097d311ef24471e
ayca_EkstrAi <ayca@ekstrai.io>
2021-02-05 06:05:29 +0300

    initial commit

---
+++ src/main/java/io/ekstrai/poc/IngestService/IngestServiceApplication.java
@@ -0,0 +1,13 @@
+package io.ekstrai.poc.IngestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngestServiceApplication.class, args);
+	}
+
+}
